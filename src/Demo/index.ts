5e0dc5469f3c23005405d3f2c9873542dba477fe
Jad2Wizard <526654076@qq.com>
2024-04-08 20:36:43 +0800

    ...

--- src/Demo/index.ts
+++ src/Demo/index.ts
@@ -1,9 +1,9 @@
-import { PositionalAudio } from 'three'
-
 async function main() {
 	const canvas = document.querySelector('#canvas') as HTMLCanvasElement
-	canvas.width = canvas.offsetWidth
-	canvas.height = canvas.offsetHeight
+	canvas.width = canvas.offsetWidth / 32
+	canvas.height = canvas.offsetHeight / 32
+
+	console.log(canvas.width, canvas.height)
 
 	const ctx = canvas.getContext('webgpu') || null
 	if (!ctx) return
@@ -15,51 +15,64 @@ async function main() {
 	})
 	if (!device) return
 
-	const num = 10000
-	const radius = 10
+	const num = 2
+	const radius = 3
 	const points = new Float32Array(num * 2)
 
-	for (let i = 0; i < num; ++i) {
-		points[i * 2] = Math.random() * 2 - 1
-		points[i * 2 + 1] = Math.random() * 2 - 1
-	}
+	points[0] = -0.99
+	points[1] = 0
+	points[2] = 0.99
+	points[3] = 0
+	// for (let i = 0; i < num; ++i) {
+	// 	points[i * 2] = Math.random() * 2 - 1
+	// 	points[i * 2 + 1] = Math.random() * 2 - 1
+	// }
 
 	const shaderModule = device.createShaderModule({
 		label: 'compute shader demo',
 		code: `
-			@group(0) @binding(0) var<storage> input: array<f32>;
-			@group(0) @binding(1) var<storage, read_write> output: array<f32>;
+			@group(0) @binding(0) var<storage> input: array<vec2f>;
+			@group(0) @binding(1) var<storage, read_write> output_x: array<atomic<u32>>;
 			@group(0) @binding(2) var<uniform> grid: vec2u;
 			@group(0) @binding(3) var<uniform> resolution: vec2f;
 			@group(0) @binding(4) var<uniform> radius: f32;
+			@group(0) @binding(5) var<storage, read_write> output_y: array<atomic<u32>>;
 
 			fn getIndex(id: vec2u) -> u32 {
 				return (id.y % grid.y) * grid.x + (id.x % grid.x);
 			}
 
-			@compute @workgroup_size(8, 8, 1)
+			@compute @workgroup_size(1, 1, 1)
 			fn main(@builtin(global_invocation_id) id: vec3u){
 				let index = getIndex(id.xy);
+				// if(index >= arrayLength(&input) / 2){
+				// 	return;
+				// }
 				let point = input[index];
 				//将 ndc 坐标系下的 point 坐标转换为屏幕空间的像素坐标，其中像素坐标的原点位于屏幕的左下方
 				let pc = (point + vec2f(1, 1)) / 2.0f * resolution;
 
+				_ = radius;
+				atomicStore(&output_x[index], u32(pc.x));
+				atomicStore(&output_y[index], u32(pc.y));
+
 				//遍历 point 像素半径覆盖的各个像素
-				for(var i = i32(-radius); i <= i32(radius); i++){
-					for(var j = i32(-radius); j <= i32(radius); j++){
-						var x = (f32(i) + pc.x) % resolution.x;
-						var y = (f32(j) + pc.y) % resolution.y;
-						x = step(0, x) * x;
-						y = step(0, y) * y;
-						let d = pow(pow(x, 2f) + pow(y, 2f), 0.5);
-						let v = step(d, f32(radius)) * d;
-						let outIdx = u32(y * resolution.x + x);
-						storageBarrier();
-						let old = output[outIdx];
-						output[outIdx] += v;
-						storageBarrier();
-					}
-				}
+				// let r = i32(radius);
+				// let r2 = pow(radius, 2f);
+				// for(var i = -r; i <= r; i++){
+				// 	for(var j = -r; j <= r; j++){
+				// 		var xx = (f32(i) + pc.x) % resolution.x;
+				// 		var yy = (f32(j) + pc.y) % resolution.y;
+				// 		let x = u32(step(0, xx) * xx);
+				// 		let y = u32(step(0, yy) * yy);
+				// 		let d = pow(f32(i), 2) + pow(f32(j), 2);
+				// 		let v = u32(step(d, r2)) * vec2u(u32(r - abs(i)), u32(r - abs(j)));
+				// 		// let v = step(0, 1 - d / radius) * (1 - d / radius);
+				// 		let outIdx = y * u32(resolution.x) + x;
+				// 		atomicAdd(&output_x[outIdx], v.x);
+				// 		atomicAdd(&output_y[outIdx], v.y);
+				// 	}
+				// }
 			}
 		`
 	})
@@ -79,9 +92,12 @@ async function main() {
 		size: radiusValue.byteLength,
 		usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
 	})
+	device.queue.writeBuffer(radiusBuffer, 0, radiusValue)
 
-	const grid = Math.ceil(num ** 0.5)
-	const gridValue = new Uint32Array([grid, grid])
+	const countX = Math.ceil(num ** 0.5)
+	const countY = Math.ceil(num / countX)
+	console.log(countX, countY, num)
+	const gridValue = new Uint32Array([countX, countY])
 	const gridBuffer = device.createBuffer({
 		label: 'Grid Uniforms',
 		size: gridValue.byteLength,
@@ -104,15 +120,27 @@ async function main() {
 	})
 	device.queue.writeBuffer(inputBuffer, 0, points)
 
-	const outputBuffer = device.createBuffer({
+	const outputBufferX = device.createBuffer({
 		label: 'output storage buffer',
 		size: canvas.width * canvas.height * 4,
 		usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
 	})
 
-	const readBuffer = device.createBuffer({
+	const readBufferX = device.createBuffer({
 		label: 'unmaped buffer',
-		size: outputBuffer.size,
+		size: outputBufferX.size,
+		usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+	})
+
+	const outputBufferY = device.createBuffer({
+		label: 'output storage buffer',
+		size: canvas.width * canvas.height * 4,
+		usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+	})
+
+	const readBufferY = device.createBuffer({
+		label: 'unmaped buffer',
+		size: outputBufferX.size,
 		usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
 	})
 
@@ -120,10 +148,11 @@ async function main() {
 		layout: pipeline.getBindGroupLayout(0),
 		entries: [
 			{ binding: 0, resource: { buffer: inputBuffer } },
-			{ binding: 1, resource: { buffer: outputBuffer } },
+			{ binding: 1, resource: { buffer: outputBufferX } },
 			{ binding: 2, resource: { buffer: gridBuffer } },
 			{ binding: 3, resource: { buffer: resolutionBuffer } },
-			{ binding: 4, resource: { buffer: radiusBuffer } }
+			{ binding: 4, resource: { buffer: radiusBuffer } },
+			{ binding: 5, resource: { buffer: outputBufferY } }
 		]
 	})
 
@@ -131,11 +160,28 @@ async function main() {
 	const computePass = encoder.beginComputePass()
 	computePass.setPipeline(pipeline)
 	computePass.setBindGroup(0, bindGroup)
-	computePass.dispatchWorkgroups(Math.ceil(grid / 8), Math.ceil(grid / 8))
-	computePass.end()
 
+	computePass.dispatchWorkgroups(countX, countY)
+	computePass.end()
+	encoder.copyBufferToBuffer(outputBufferX, 0, readBufferX, 0, readBufferX.size)
+	encoder.copyBufferToBuffer(outputBufferY, 0, readBufferY, 0, readBufferY.size)
 	const commandBuffer = encoder.finish()
 	device.queue.submit([commandBuffer])
+
+	await readBufferX.mapAsync(GPUMapMode.READ)
+	await readBufferY.mapAsync(GPUMapMode.READ)
+	const data = new Uint32Array(readBufferX.getMappedRange())
+	const data1 = new Uint32Array(readBufferY.getMappedRange())
+	let count = 0
+	for (let i = 0; i < data.length; ++i) {
+		if (data[i] !== 0) {
+			count++
+			console.log(i, i % canvas.width, (i / canvas.width) | 0, data[i], data1[i])
+		}
+	}
+	console.log(data)
+	console.log(data1)
+	console.log('count: ' + count)
 }
 
 main()

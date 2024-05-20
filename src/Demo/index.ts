19eebdd8c1803b4cfa240f0967af2eda384689e1
Jad2Wizard <526654076@qq.com>
2024-05-20 10:27:23 +0800

    demo:  测试使用 format: 'rgba16float'纹理作为渲染的输出纹理,并作为下一个 renderpass 的输入纹理渲染到 canvas 上. 用计算着色器计算热力图上各个像素的热力值时,因为原子操作的引入导致渲染一帧耗时过长.考虑通过离屏渲染的方式在片元着色器中计算像素热力值,并在混合阶段累加热力值

--- src/Demo/index.ts
+++ src/Demo/index.ts
@@ -31,18 +31,46 @@ export async function main(canvas: HTMLCanvasElement) {
 		}
 
 		@fragment fn fs() -> @location(0) vec4f {
-			return vec4f(1, 1, 1, 1);
+			return vec4f(0.5, 0.2, 0, 1);
 		}
 	`
+
+	const deferredRenderCode = `
+		@vertex fn vs(@builtin(vertex_index) vi: u32) -> @builtin(position) vec4f {
+			let pos = array(
+				vec2f(-1.0, 1.0),
+				vec2f(-1.0, -1.0),
+				vec2f(1.0, -1.0),
+				vec2f(1.0, -1.0),
+				vec2f(1.0, 1.0),
+				vec2f(-1.0, 1.0)
+			);
+
+			let p = vec4f(pos[vi % 6], 0, 1);
+			return p;
+		}
+
+		@group(0) @binding(0) var tex: texture_2d<f32>;
+		@fragment fn fs(@builtin(position) coord: vec4f) ->  @location(0) vec4f {
+			let color = textureLoad(tex, vec2i(floor(coord.xy)), 0).rgb;
+			return vec4f(color, 1);
+		}
+	`
+
 	const module = device.createShaderModule({
 		label: 'triangle vertex shader with uniforms',
 		code
 	})
 
+	const deferredModule = device.createShaderModule({
+		label: 'deferred render module',
+		code: deferredRenderCode
+	})
+
 	const texture = device.createTexture({
 		size: [canvas.width, canvas.height, 1],
 		format: 'rgba16float',
-		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING
 	})
 
 	const pipeline = device.createRenderPipeline({
@@ -72,7 +100,38 @@ export async function main(canvas: HTMLCanvasElement) {
 			]
 		}
 	})
-	console.log(presentationFormat)
+
+	const deferredPipeline = device.createRenderPipeline({
+		label: 'deferred pipeline',
+		layout: device.createPipelineLayout({
+			bindGroupLayouts: [
+				device.createBindGroupLayout({
+					entries: [
+						{
+							binding: 0,
+							visibility: GPUShaderStage.FRAGMENT,
+							texture: {
+								sampleType: 'unfilterable-float'
+							}
+						}
+					]
+				})
+			]
+		}),
+		vertex: {
+			module: deferredModule,
+			entryPoint: 'vs'
+		},
+		fragment: {
+			module: deferredModule,
+			entryPoint: 'fs',
+			targets: [
+				{
+					format: presentationFormat
+				}
+			]
+		}
+	})
 
 	const renderPassDescriptor: GPURenderPassDescriptor = {
 		label: 'our basic canvas renderPass',
@@ -86,11 +145,33 @@ export async function main(canvas: HTMLCanvasElement) {
 		]
 	}
 
-	const bytesPerRow = Math.ceil((canvas.width * 4 * 2) / 256) * 256
-	const readBuffer = device.createBuffer({
-		size: bytesPerRow * canvas.height,
-		usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+	const deferredRenderPassDescriptor: GPURenderPassDescriptor = {
+		label: 'deferred render pass desc',
+		colorAttachments: [
+			{
+				view: ctx.getCurrentTexture().createView(),
+				clearValue: [0, 0, 0, 1],
+				loadOp: 'clear',
+				storeOp: 'store'
+			}
+		]
+	}
+
+	const bindGroup = device.createBindGroup({
+		layout: deferredPipeline.getBindGroupLayout(0),
+		entries: [
+			{
+				binding: 0,
+				resource: texture.createView()
+			}
+		]
 	})
+
+	// const bytesPerRow = Math.ceil((canvas.width * 4 * 2) / 256) * 256
+	// const readBuffer = device.createBuffer({
+	// 	size: bytesPerRow * canvas.height,
+	// 	usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+	// })
 
 	async function render() {
 		if (!device || !ctx) return
@@ -104,21 +185,27 @@ export async function main(canvas: HTMLCanvasElement) {
 		pass.draw(6)
 		pass.end()
 
-		encoder.copyTextureToBuffer(
-			{ texture },
-			{
-				buffer: readBuffer,
-				bytesPerRow
-			},
-			[canvas.width, canvas.height, 1]
-		)
+		const deferredPass = encoder.beginRenderPass(deferredRenderPassDescriptor)
+		deferredPass.setPipeline(deferredPipeline)
+		deferredPass.setBindGroup(0, bindGroup)
+		deferredPass.draw(6)
+		deferredPass.end()
+
+		// encoder.copyTextureToBuffer(
+		// 	{ texture },
+		// 	{
+		// 		buffer: readBuffer,
+		// 		bytesPerRow
+		// 	},
+		// 	[canvas.width, canvas.height, 1]
+		// )
 		const commandBuffer = encoder.finish()
 		device.queue.submit([commandBuffer])
 
-		await readBuffer.mapAsync(GPUMapMode.READ)
-		const arrayBuffer = readBuffer.getMappedRange()
-		const data = new Uint16Array(arrayBuffer)
-		console.log(data)
+		// await readBuffer.mapAsync(GPUMapMode.READ)
+		// const arrayBuffer = readBuffer.getMappedRange()
+		// const data = new Uint16Array(arrayBuffer)
+		// console.log(data)
 	}
 
 	render()

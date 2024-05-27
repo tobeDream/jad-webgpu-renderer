c13bc4ce7d25e84529b54904bf0604539e0842e1
Jad2Wizard <526654076@qq.com>
2024-05-27 18:23:25 +0800

    ...

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import Scene from './Scene'
 import Model from './Model'
 import Points from './Points'
 import Heatmap from './Heatmap'
+import Line from './Line'
+import { Camera } from './camera/camera'
 import Material from './material/material'
 import Uniform from './material/uniform'
 import Geometry from './geometry/geometry'
 import Attribute from './geometry/attribute'
 
-export { Renderer, Scene, Model, Points, Material, Uniform, Geometry, Attribute }
+export { Renderer, Scene, Camera, Model, Points, Line, Heatmap, Material, Uniform, Geometry, Attribute }
